1c2584c2a494106f46dac2ea92f421ae082ff3f3
Seawis <sergio55@yandex.ru>
2025-10-10 09:46:28 +0600

    StageLast1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import loader from './src/loader.js'
-import { defaultDir } from './src/filePaths.js'
+import { defaultDir } from './src/loadPaths.js'
 
 export default (url, output = defaultDir('tmp')) => loader(url, output)
