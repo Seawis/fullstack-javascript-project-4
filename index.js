62b3f2a7631ca42c68ae90912781246b468ecc6e
Seawis <sergio55@yandex.ru>
2025-10-15 10:24:05 +0600

    processCwd index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import loader from './src/loader.js'
-import { defaultDir } from './src/loadPaths.js'
+// import { defaultDir } from './src/loadPaths.js'
 
-export default (url, output = defaultDir('tmp')) => loader(url, output)
+export default (url, output = process.cwd()) => loader(url, output) // defaultDir('tmp')
